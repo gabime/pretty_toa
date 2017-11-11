f4898d0e23c903055877f5dcb40acf13b32a5f7b
gabime <gmelman1@gmail.com>
2017-11-11 15:27:28 +0200

    typo

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@
 
 
 //!
-//! Fast convert numbers to strings with thousands separator.
+//! Fast convert numbers to strings with thousands separators.
 //!
 //! It is very fast because it uses itoa/dtoa and stack allocated strings.
 //!
